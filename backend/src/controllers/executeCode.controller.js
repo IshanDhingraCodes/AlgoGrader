f828d858379a112c418f0d502724e8a5b022097d
ishan <ishan.dhingra.work@gmail.com>
2025-05-17 17:49:10 +0530

    Feat: complete the contoller for storing submission, problem solved and individual test case results in the backend

--- backend/src/controllers/executeCode.controller.js
+++ backend/src/controllers/executeCode.controller.js
@@ -1,6 +1,12 @@
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiError } from "../utils/api-error.js";
-import { pollBatchResults, submitBatch } from "../libs/judge0.lib.js";
+import { ApiResponse } from "../utils/api-response.js";
+import {
+  getLanguageName,
+  pollBatchResults,
+  submitBatch,
+} from "../libs/judge0.lib.js";
+import { db } from "../libs/db.js";
 
 export const executeCode = asyncHandler(async (req, res) => {
   const { source_code, language_id, stdin, expected_outputs, problemId } =
@@ -42,7 +48,112 @@ export const executeCode = asyncHandler(async (req, res) => {
   console.log("Result__________");
   console.log(results);
 
-  return res.status(200).json({
-    message: "Code Executed",
+  //Analyze test case result
+  let allPassed = true;
+
+  const detailedResults = results.map((result, i) => {
+    const stdout = result.stdout?.trim();
+    const expected_output = expected_outputs[i]?.trim();
+    const passed = stdout === expected_output;
+
+    if (!passed) allPassed = false;
+
+    return {
+      testCase: i + 1,
+      passed,
+      stdout,
+      expected: expected_output,
+      stderr: result.stderr || null,
+      compile_output: result.compile_output || null,
+      status: result.status.description,
+      memory: result.memory ? `${result.memory} KB` : undefined,
+      time: result.time ? `${result.time} s` : undefined,
+    };
+
+    // console.log(`Testcase #${i+1}--------`)
+    // console.log(`Input for testcase #${i+1}: ${stdin[i]}`)
+    // console.log(`Expected Output for testcase #${i+1}: ${expected_output}`)
+    // console.log(`Actual output for testcase #${i+1}: ${stdout}`)
+    // console.log(`Matched testcase #${i+1}: ${passed}`)
   });
+
+  const submission = await db.submission.create({
+    data: {
+      userId,
+      problemId,
+      sourceCode: source_code,
+      language: getLanguageName(language_id),
+      stdin: stdin.join("\n"),
+      stdout: JSON.stringify(detailedResults.map((r) => r.stdout)),
+      stderr: detailedResults.some((r) => r.stderr)
+        ? JSON.stringify(detailedResults.map((r) => r.stderr))
+        : null,
+      compileOutput: detailedResults.some((r) => r.compile_output)
+        ? JSON.stringify(detailedResults.map((r) => r.compile_output))
+        : null,
+      status: allPassed ? "Accepted" : "Wrong Answer",
+      memory: detailedResults.some((r) => r.memory)
+        ? JSON.stringify(detailedResults.map((r) => r.memory))
+        : null,
+      time: detailedResults.some((r) => r.time)
+        ? JSON.stringify(detailedResults.map((r) => r.time))
+        : null,
+    },
+  });
+
+  //If all passed = true mark problem as solved for the current user
+
+  if (allPassed) {
+    await db.problemSolved.upsert({
+      where: {
+        userId_problemId: {
+          userId,
+          problemId,
+        },
+      },
+      update: {},
+      create: {
+        userId,
+        problemId,
+      },
+    });
+  }
+
+  // save individual test case results using detailedResult
+
+  const testCaseResults = detailedResults.map((result) => ({
+    submissionId: submission.id,
+    testCase: result.testCase,
+    passed: result.passed,
+    stdout: result.stdout,
+    expected: result.expected,
+    stderr: result.stderr,
+    compileOutput: result.compile_output,
+    status: result.status,
+    memory: result.memory,
+    time: result.time,
+  }));
+
+  await db.testCaseResult.createMany({
+    data: testCaseResults,
+  });
+
+  const submissionWithTestCase = await db.submission.findUnique({
+    where: {
+      id: submission.id,
+    },
+    include: {
+      testCases: true,
+    },
+  });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { submission: submissionWithTestCase },
+        "Code Executed Successfully.",
+      ),
+    );
 });
