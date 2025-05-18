bea949579c77ca904ec0d4b6e182d8af478d67b3
ishan <ishan.dhingra.work@gmail.com>
2025-05-18 13:18:06 +0530

    Feat: created controller for fetching problems solved by user

--- backend/src/controllers/problem.controller.js
+++ backend/src/controllers/problem.controller.js
@@ -247,8 +247,30 @@ export const deleteProblem = asyncHandler(async (req, res) => {
 
   await db.problem.delete({ where: { id } });
 
-  return res.status(200).json(new ApiResponse(200, null, "Problem deleted successfully."))
-
+  return res
+    .status(200)
+    .json(new ApiResponse(200, null, "Problem deleted successfully."));
 });
 
-export const getAllProblemsSolvedByUser = asyncHandler(async (req, res) => {});
+export const getAllProblemsSolvedByUser = asyncHandler(async (req, res) => {
+  const problems = await db.problem.findMany({
+    where: {
+      solvedBy: {
+        some: {
+          userId: req.user.id,
+        },
+      },
+    },
+    include: {
+      solvedBy: {
+        where: {
+          userId: req.user.id,
+        },
+      },
+    },
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, problems, "Problems fetched successfully"));
+});
