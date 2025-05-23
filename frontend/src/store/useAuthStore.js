4701f234c174afd41dbef923a2fe3ad85ff541f8
ishan <ishan.dhingra.work@gmail.com>
2025-05-23 20:43:33 +0530

    Fix: Destructure error in the checkAuth state

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -14,7 +14,7 @@ export const useAuthStore = create((set) => ({
       const res = await axiosInstance.get("/auth/check");
       console.log("checkauth response", res.data);
 
-      set({ authUser: res.data.user });
+      set({ authUser: res.data.data.user });
     } catch (error) {
       console.log("❌ Error checking auth:", error);
       set({ authUser: null });
